290f8f60e1191c5f26c542dc60aaf035e0e964b2
MineBill <minebill@protonmail.com>
2025-01-25 20:29:31 +0200

    Init

---
+++ Setup/Generator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace ImGuiBeefGenerator
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine($"Working directory: {Directory.GetCurrentDirectory()}");
+            var generator = new BindingGenerator();
+            generator.Initialize();
+
+            var outputFiles = generator.Generate(true);
+
+            Console.WriteLine("Writing output files");
+
+            foreach (var file in outputFiles)
+                File.WriteAllText($"../{file.Key}", file.Value);
+        }
+    }
+}
---
+++ Setup/src/Program.bf
@@ -0,0 +1,12 @@
+using System;
+
+namespace Setup;
+
+class Program
+{
+	static int32 Main(String[] args)
+	{
+		Console.WriteLine("Fuck");
+		return 0;
+	}
+}
